28e780a51673722fcc207d3c28fd110c35fd2657
Oscar Ramirez <ing.oarrs@gmail.com>
2024-07-30 20:49:24 -0600

    f

--- src/app/core/models/invitadoRecurrente.model.ts
+++ src/app/core/models/invitadoRecurrente.model.ts
@@ -1,22 +1,13 @@
-export class Boleto {
+export class InvitadoRecurrente {
     constructor(
-        public fiesta: string,
         public email: string,
-        public cantidadInvitados: number,
-        public mesa: string,
         public grupo: string,
         public salon: string,
         public nombreGrupo: string,
         public whatsapp: string,
-        public confirmado: boolean,
-        public invitacionEnviada: boolean,
         public activated: boolean,
-        public ocupados: number,
-        public vista:boolean,
-        public pushNotification: [object],
         public dateCreated: number,
         public usuarioCreated: string,
-        public fechaConfirmacion: number,
         public lastEdited: number,
         public uid: string
     ) { }
